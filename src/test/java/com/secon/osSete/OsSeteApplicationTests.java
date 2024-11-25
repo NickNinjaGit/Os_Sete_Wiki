856a57fd5ede29ffcfb70f258a1f9b4b770fdea6
anthonySeconDuarte <anthony.duarte@etec.sp.gov.br>
2024-11-24 22:02:44 -0300

    final commit for demo version of backend

---
+++ src/test/java/com/secon/osSete/OsSeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secon.osSete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsSeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
