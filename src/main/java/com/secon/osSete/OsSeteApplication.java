8e9ded90910eba4970191627cb345dfa9ba67343
anthonySeconDuarte <anthony.duarte@etec.sp.gov.br>
2024-11-24 21:41:58 -0300

    initial commit

---
+++ src/main/java/com/secon/osSete/OsSeteApplication.java
@@ -0,0 +1,13 @@
+package com.secon.osSete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsSeteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsSeteApplication.class, args);
+	}
+
+}
